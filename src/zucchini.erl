196afcf63e427a18f20f4b729764e61f4d3ebd47
Devin Torres <devin@devintorres.com>
2011-02-24 18:03:57 -0600

    Make sure parse_string is passed either a binary or a list

--- src/zucchini.erl
+++ src/zucchini.erl
@@ -4,7 +4,7 @@
 
 parse_string(String) when is_binary(String) ->
     parse_string(binary_to_list(String));
-parse_string(String) ->
+parse_string(String) when is_list(String) ->
     {ok, Tokens, _} = zucchini_lexer:string(String),
     zucchini_parser:parse(Tokens).
 
